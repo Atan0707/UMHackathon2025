c2d9ecab744fc63d30ff867a37c0c76fa016bcfa
Hariz Hakim <harizhakim85@gmail.com>
2025-04-19 16:24:40 +0800

    update endpoint

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
 export const WALLETCONNECT_PROJECT_ID = "b5afe545e87787c53896449028cc2ad7";
 export const CONTRACT_ADDRESS = "0xF552aA1729fDb7DcFA1FBedE2834Db6569f0ed66";
 export const SUBGRAPH_URL =
-  "https://api.studio.thegraph.com/query/106159/umhackathonv2/version/latest";
+  "https://api.studio.thegraph.com/query/106159/umhackathonv3/version/latest";
 export const RPC_URL =
   "https://scroll-sepolia.g.alchemy.com/v2/7z_rnkKFONf8r6Adr1poXg1qugZlhclq";
