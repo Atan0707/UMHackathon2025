7a1d118ff81a1a6cb0b0e16e5a8216ab091e4cfd
Hariz Hakim <harizhakim85@gmail.com>
2025-04-20 02:41:45 +0800

    update subgraph

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,7 +1,7 @@
 export const WALLETCONNECT_PROJECT_ID = "b5afe545e87787c53896449028cc2ad7";
 export const CONTRACT_ADDRESS = "0xF552aA1729fDb7DcFA1FBedE2834Db6569f0ed66";
 export const SUBGRAPH_URL =
-  "https://api.studio.thegraph.com/query/106159/umhackathonv3/version/latest";
+  "https://api.studio.thegraph.com/query/106159/umhackathonv4/version/latest";
 export const RPC_URL =
   "https://scroll-sepolia.g.alchemy.com/v2/7z_rnkKFONf8r6Adr1poXg1qugZlhclq";
 export const ZAKAT_NFT_CONTRACT_ADDRESS = "0xCf46E80e77D78c9D3726A254E60296CAD42cc9bF";
