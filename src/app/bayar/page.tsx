9c17600861bbc51e62f9a217b8de506fa3f73326
Izhan <zikrizhan@gmail.com>
2025-04-20 05:03:37 +0800

    update ui

--- src/app/bayar/page.tsx
+++ src/app/bayar/page.tsx
@@ -435,10 +435,6 @@ export default function BayarPage() {
     setSelectedBank(bank);
   };
 
-  const downloadReceipt = () => {
-    setIsReceiptModalOpen(true);
-  };
-
   const handlePrintReceipt = () => {
     const receiptContent = receiptRef.current;
     if (!receiptContent) return;
@@ -1467,7 +1463,7 @@ export default function BayarPage() {
               </div>
             </div>
             <div className="mt-6 flex justify-center space-x-4">
-              <button
+              {/* <button
                 className="px-6 py-2 bg-emerald-600 text-white rounded-md hover:bg-emerald-700 flex items-center justify-center"
                 onClick={downloadReceipt}
               >
@@ -1475,7 +1471,7 @@ export default function BayarPage() {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
                 </svg>
                 Muat Turun Resit
-              </button>
+              </button> */}
               <Link href="/" className="px-6 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 flex items-center justify-center">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
@@ -1577,9 +1573,9 @@ export default function BayarPage() {
             </button>
           ) : (
             <div className="w-full flex justify-center">
-              <Link href="/" className="px-6 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700">
+              {/* <Link href="/" className="px-6 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700">
                 KEMBALI KE LAMAN UTAMA
-              </Link>
+              </Link> */}
             </div>
           )}
 
